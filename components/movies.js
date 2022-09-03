6b63d23730247d4b6beadb41f05c7d905df1926f
Victor Fernandes <vitorino.fernandes@avital.in>
2022-09-03 13:41:07 +0530

    fix

--- components/movies.js
+++ components/movies.js
@@ -26,9 +26,35 @@ const MoviesListing = ({genere, page}) => {
         dots: false,
         infinite: false,
         speed: 500,
-        slidesToShow: 6,
-        slidesToScroll: 1
-      };
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow:2,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    };
 
     useEffect(()=>{
         fetchMovies();
